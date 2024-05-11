c20eb8dc50c24846cdb699b63610afea4a0cd066
ldh-Hoon <139981434+ldh-Hoon@users.noreply.github.com>
2024-05-11 23:11:52 +0900

    Update MAILABS_dataset.py

--- src/datasets/MAILABS_dataset.py
+++ src/datasets/MAILABS_dataset.py
@@ -3,9 +3,10 @@ import pandas as pd
 from pathlib import Path
 
 class MAILABS(SimpleAudioFakeDataset):                                            
-    def __init__(self, root_path, **kwargs):
-        super().__init__(root_path, **kwargs)
-        self.root_path = Path(f'{root_path}/MAILABS')
+    def __init__(self, root_path, subset=None, **kwargs):
+        super().__init__(root_path, subset, **kwargs)
+        self.root_path = Path(f'{root_path}')
+        self.subset = subset
         self.samples = self.load_samples()
 
     def load_samples(self):
@@ -19,12 +20,14 @@ class MAILABS(SimpleAudioFakeDataset):
 
         path = self.root_path / ""
         
-        # 해당 언어의 디렉토리가 존재하는지 확인
         if not path.exists():
             print(f"{path} 경로를 찾을 수 없습니다.")
         
         samples_list = list(path.rglob("*.wav"))
-        samples_list = self.split_samples(samples_list)
+        if self.subset == 'train':
+            samples_list = samples_list[:int(len(samples_list)*0.7)]
+        else:
+            samples_list = samples_list[int(len(samples_list)*0.7):]
         for sample in samples_list:
             samples["user_id"].append(None)
             samples["path"].append(sample)
