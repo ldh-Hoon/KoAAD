9a5e95c275a7f50f92efd57b6a410950b408794a
ldh-Hoon <139981434+ldh-Hoon@users.noreply.github.com>
2024-05-22 12:43:51 +0900

    Update MAILABS_dataset.py

--- src/datasets/MAILABS_dataset.py
+++ src/datasets/MAILABS_dataset.py
@@ -18,24 +18,31 @@ class MAILABS(SimpleAudioFakeDataset):
             "path": []
         }
 
-        path = self.root_path / ""
-        
-        if not path.exists():
-            print(f"{path} 경로를 찾을 수 없습니다.")
-        
-        samples_list = list(path.rglob("*.wav"))
-        if self.subset == 'train':
-            samples_list = samples_list[:int(len(samples_list)*0.7)]
-            print(f"__MAILABS_train:{len(samples_list)}")
-        else:
-            samples_list = samples_list[int(len(samples_list)*0.7):]
-            print(f"__MAILABS_test:{len(samples_list)}")
-        for sample in samples_list:
-            samples["user_id"].append(None)
-            samples["path"].append(sample)
-            samples["sample_name"].append(sample.stem)
-            samples["attack_type"].append("-")
-            samples["label"].append("bonafide")
+        folders_1 = list(self.root_path.glob("en_US/by_book/*"))
+        for f1 in folders_1:
+            if not os.path.isdir(f1):
+                continue
+            folders_2 = list(f1.glob("*"))
+            for f2 in folders_2:
+                path = f1 / f2.name
+
+                if not path.exists():
+                    print(f"{path} 경로를 찾을 수 없습니다.")
+
+                samples_list = list(path.rglob("*.wav"))
+                if self.subset == 'train':
+                    samples_list = samples_list[:int(len(samples_list)*split[0])]
+                elif self.subset == 'test':
+                    samples_list = samples_list[int(len(samples_list)*(split[0]):]
+                for sample in samples_list:
+                    if sample.stem[0]==".":
+                        continue
+                    if os.path.exists(sample):
+                        samples["user_id"].append(None)
+                        samples["path"].append(sample)
+                        samples["sample_name"].append(sample.stem)
+                        samples["attack_type"].append("-")
+                        samples["label"].append("bonafide")
 
         return pd.DataFrame(samples)
 
