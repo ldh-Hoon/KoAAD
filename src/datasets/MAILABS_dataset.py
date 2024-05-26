c4a0c8f17506fcf5c87ec2b36e8df9a735574ee7
ldh-Hoon <139981434+ldh-Hoon@users.noreply.github.com>
2024-05-26 20:05:55 +0900

    Update MAILABS_dataset.py

--- src/datasets/MAILABS_dataset.py
+++ src/datasets/MAILABS_dataset.py
@@ -19,32 +19,33 @@ class MAILABS(SimpleAudioFakeDataset):
             "path": []
         }
         split = [0.7, 0.3]
-
-        folders_1 = list(self.root_path.glob("en_US/by_book/*"))
-        for f1 in folders_1:
-            if not os.path.isdir(f1):
-                continue
-            folders_2 = list(f1.glob("*"))
-            for f2 in folders_2:
-                path = f1 / f2.name
-
-                if not path.exists():
-                    print(f"{path} 경로를 찾을 수 없습니다.")
-
-                samples_list = list(path.rglob("*.wav"))
-                if self.subset == 'train':
-                    samples_list = samples_list[:int(len(samples_list)*split[0])]
-                elif self.subset == 'test':
-                    samples_list = samples_list[int(len(samples_list)*(split[0])):]
-                for sample in samples_list:
-                    if sample.stem[0]==".":
-                        continue
-                    if os.path.exists(sample):
-                        samples["user_id"].append(None)
-                        samples["path"].append(sample)
-                        samples["sample_name"].append(sample.stem)
-                        samples["attack_type"].append("-")
-                        samples["label"].append("bonafide")
+        folders_lang = list(self.root_path.glob("*"))
+        for f_lang in folders_lang:
+            folders_1 = list(self.root_path.glob(f"{f_lang.name}/by_book/*"))
+            for f1 in folders_1:
+                if not os.path.isdir(f1):
+                    continue
+                folders_2 = list(f1.glob("*"))
+                for f2 in folders_2:
+                    path = f1 / f2.name
+    
+                    if not path.exists():
+                        print(f"{path} 경로를 찾을 수 없습니다.")
+    
+                    samples_list = list(path.rglob("*.wav"))
+                    if self.subset == 'train':
+                        samples_list = samples_list[:int(len(samples_list)*split[0])]
+                    elif self.subset == 'test':
+                        samples_list = samples_list[int(len(samples_list)*(split[0])):]
+                    for sample in samples_list:
+                        if sample.stem[0]==".":
+                            continue
+                        if os.path.exists(sample):
+                            samples["user_id"].append(None)
+                            samples["path"].append(sample)
+                            samples["sample_name"].append(sample.stem)
+                            samples["attack_type"].append("-")
+                            samples["label"].append("bonafide")
         print(f"MAILABS_{self.subset}:{len(samples['label'])}")
         return pd.DataFrame(samples)
 
